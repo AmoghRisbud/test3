bd262095284f95b7ca6cad1cc3ab565bf6f53f06
Amogh <amoghrisbud28@gmail.com>
2020-11-25 12:34:36 +0530

    modified

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 
 
 
-/*function getdata(){
-    fetch("https://apis.solarialabs.com/shine/v1/vehicle-thefts?state=UT&apikey=123456789ABCDEFGHIJK", {
-	"method": "GET"
-})
+function getdata(){
+    fetch("http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=e055443a32f0471398420ff3ca57bd4d")
 .then(response => {
 	console.log(response);
 })
 .catch(err => {
-	//console.error(err);
+	console.error(err);
 });
 }
 //setInterval(getdata,1800000);*/
